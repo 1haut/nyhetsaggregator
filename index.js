a0c54f42ea8aec39ddc1d041f3ab3ee26c23b6d7
1haut <ozioma.marshall.anike@funkweb.org>
2025-02-25 15:21:25 +0100

    Hente overskrifter fra NRK og VG

--- index.js
+++ index.js
@@ -3,21 +3,58 @@
 // import puppeteer from "puppeteer";
 import readline from 'node:readline/promises';
 
-const rl = readline.createInterface({
+async function emneValg() { 
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-
-async function emneValg() {     
-      const brukerSvar = await rl.question('Hvilke emner har du lyst til å lese om? ');
-      const stikkord = brukerSvar.split(",")
-      rl.close()
-      
-      console.log("Du har valgt å lese om: ")
-      stikkord.forEach(emne => {
-          emne = emne.trim()
-          console.log(emne)
-      });
+  
+  const brukerSvar = await rl.question('Hvilke emner har du lyst til å lese om? ');
+  const stikkord = brukerSvar.split(",")
+  rl.close()
+  
+  return stikkord
 }
 
-emneValg()
+function scrapeVgForside() {
+    try {
+        const $ = cheerio.load("https://www.vg.no");
+        const data = [];
+        $("article:not([hidden]) .titles").each((index, element) => {
+            let overskrift = $(element).text();
+            overskrift = overskrift.replace(/\s*\n\s*/g, ' ').trim("");
+            data.push(overskrift);
+        })
+
+        console.log(data);
+    } catch(err) {
+        console.error("Feil ved henting av data: ", err);
+    }
+}
+
+function scrapeNrkForside() {
+    try {
+        const $ = cheerio.load("https://www.nrk.no");
+        const data = [];
+        $('kur-room__title').each((index, element) => {
+            let overskrift = $(element).text().trim("");
+            data.push(overskrift);
+        })
+        console.log(data.length, data);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+function main() {
+  const emner = emneValg()
+
+  console.log("Du har valgt å lese om: ")
+  emner.forEach(emne => {
+      emne = emne.trim()
+      console.log(emne)
+  });
+}
+
+main()
